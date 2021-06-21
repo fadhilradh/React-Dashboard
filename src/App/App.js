37aa43ac77bed3cceccc305045f8bf3478645eae
Fadhil Radhian <fadhilradhian@gmail.com>
2021-06-21 21:46:20 +0700

    init

---
+++ src/App/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Sidemenu from "../components/SideMenu";
+
+function App() {
+  return (
+    <div className="App">
+      <Sidemenu />
+    </div>
+  );
+}
+
+export default App;
