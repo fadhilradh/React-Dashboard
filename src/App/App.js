fe4f9547e7189bb6d02bb152648e48271c4cd0e7
Fadhil Radhian <fadhilradhian@gmail.com>
2021-07-04 12:14:30 +0700

    employee cms

--- src/App/App.js
+++ src/App/App.js
@@ -2,11 +2,12 @@ import "./App.css";
 import Sidemenu from "../components/SideMenu";
 
 function App() {
-  return (
-    <div className="App">
-      <Sidemenu />
-    </div>
-  );
+    return (
+        <div className="App">
+            <Header />
+            <Sidemenu />
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import { AppBar, Toolbar } from "@material-ui/core";
+
+const Header = () => {
+    return (
+        <AppBar position="sticky">
+            <Toolbar></Toolbar>
+        </AppBar>
+    );
+};
+
+export default Header;
